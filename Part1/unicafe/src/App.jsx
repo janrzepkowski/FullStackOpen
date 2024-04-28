f59334cce207b796c85b6046dc8a6eef3e90c534
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-04-28 14:37:15 +0200

    1.9

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react'
 
 const Statistics = (props) => {
   let all = props.good + props.neutral + props.bad
+
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>statistics</h1>
