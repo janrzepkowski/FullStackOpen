5279a64ad3cb02def2a9dda86665fc090a6d5d23
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-08-10 11:38:08 +0200

    2.19*: Data for countries, step 2

--- Part2/countries/src/components/Countries.jsx
+++ Part2/countries/src/components/Countries.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import Country from "./country";
 
 const Countries = ({ countries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const handleShowClick = (country) => {
+    setSelectedCountry(country);
+  };
+
+  if (selectedCountry) {
+    return <Country country={selectedCountry} />;
+  }
+
   if (countries.length === 1) {
     return <Country country={countries[0]} />;
   }
@@ -12,7 +23,10 @@ const Countries = ({ countries }) => {
   return (
     <div>
       {countries.map((country) => (
-        <div key={country.cca2}>{country.name.common}</div>
+        <div key={country.cca2}>
+          {country.name.common}{" "}
+          <button onClick={() => handleShowClick(country)}>show</button>
+        </div>
       ))}
     </div>
   );
