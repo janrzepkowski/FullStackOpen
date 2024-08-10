1ee6e97c5f4cdc6421c7196820ed11089ebb1535
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-08-10 09:18:50 +0200

    Filter component & getAll service

--- Part2/countries/src/App.jsx
+++ Part2/countries/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
+import countriesService from "./services/countries";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  useEffect(() => {
+    countriesService.getAll().then((initialCountries) => {
+      setCountries(initialCountries);
+    });
+  }, []);
 
   return (
     <div>
       <h1>Countries</h1>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
     </div>
   );
-}
+};
 
 export default App;
