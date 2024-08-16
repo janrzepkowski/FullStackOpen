55d7dc95b366b3a39f72cae453022fcf2ffac1b4
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-08-16 12:57:35 +0200

    REST & Fetching a single resource

--- Part3/backend_notes/index.js
+++ Part3/backend_notes/index.js
@@ -19,13 +19,17 @@ let notes = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
