0fc5b1b2a0b16664bb9bdddd24069dde78a2222d
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-08-19 10:55:04 +0200

    3.5: new phonebook entries

--- Part3/backend_phonebook/index.js
+++ Part3/backend_phonebook/index.js
@@ -60,6 +60,26 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000).toString(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
