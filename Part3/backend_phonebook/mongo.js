5ace0a76438d12ee144d912461b23a464a5dd557
janrzepkowski <jan.rzepkowski21@gmail.com>
2024-08-26 17:57:37 +0200

    phonebook .env

--- Part3/backend_phonebook/mongo.js
+++ Part3/backend_phonebook/mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 if (process.argv.length < 3) {
   console.log("give password as argument");
@@ -9,7 +10,7 @@ const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
 
-const url = `mongodb+srv://janrzepkowski21:v8ogjM1Y0MK4xyEo@fullstackopenpart3.4hkxo.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=FullStackOpenPart3`;
+const url = process.env.MONGODB_URI;
 
 mongoose.set("strictQuery", false);
 
